ac50dd6f27562ca3625db290bcafd23077af4fa8
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-11-04 17:25:57 -0500

    dont shrink all the vanilla icons lmao what

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,8 @@ class $modify(MyGJItemIcon, GJItemIcon) {
 	void scaleGracefully(float dt) {
 		if (!enabled) return;
 		if (!m_player) return;
-		if (moreIconsOnly && (!this->getParent() || !this->getParent()->getUserObject("hiimjustin000.more_icons/name"))) return;
+		const bool isNotFromMoreIcons = !this->getParent() || !this->getParent()->getUserObject("hiimjustin000.more_icons/name");
+		if (moreIconsOnly && isNotFromMoreIcons) return;
 		auto simplePlayerChildSprite = m_player->getChildByType<CCSprite>(0);
 		if (!simplePlayerChildSprite || simplePlayerChildSprite->getChildrenCount() < 4) return;
 		const float originalScale = this->scaleForType(m_fields->unlockType);
@@ -42,12 +43,15 @@ class $modify(MyGJItemIcon, GJItemIcon) {
 				futureScale = this->getContentWidth() / simplePlayerChildSprite->getContentWidth();
 			}
 		}
-		if ((originalScale * futureScale) < originalScale) this->setScale(originalScale * futureScale);
+		if ((originalScale * futureScale) < originalScale) {
+			if (isNotFromMoreIcons) this->setScale(futureScale);
+			else this->setScale(originalScale * futureScale);
+		}
 	}
 	bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
 		if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, p7)) return false;
 		m_fields->unlockType = p0;
-		this->scheduleOnce(schedule_selector(MyGJItemIcon::scaleGracefully), .009f);
+		this->scheduleOnce(schedule_selector(MyGJItemIcon::scaleGracefully), .001f);
 		return true;
 	}
 };
